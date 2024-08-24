ad960f82a104ab774e1c6277beae74bb5599cea9
ankitgaur0 <ankitparashar000@gmail.com>
2024-08-24 17:04:38 +0530

    file is updated

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import Flask,requests,render_template
+from flask import Flask,request,render_template
 from src.pipeline.test_pipeline import Custom_data,predict_pipeline
 
 
@@ -11,20 +11,20 @@ def home_page():
 
 @app.route("/predict",methods=['GET','POST'])
 def predict_datapoints():
-    if requests.method == "GET":
+    if request.method == "GET":
         return render_template("form.html")
     else:
         DATA=Custom_data(
-                 Temperature=requests.form.get("Temperature"),
-                 Humidity= requests.form.get("Humidity"),
-                 Wind_Speed =requests.form.get("Wind_Speed"),
-                 Precipitation =requests.form.get("Precipitation"),
-                 Cloud_Cover =requests.form.get("Cloud_Cover"),
-                 Atmospheric_Pressure =requests.form.get("Atmospheric_Pressure"),
-                 UV_Index =requests.form.get("UV_Index"),
-                 Season =requests.form.get("Season"),
-                 Visibility_km =requests.form.get("Visibility_km"),
-                 Location =requests.form.get("Location")
+                 Temperature=request.form.get("Temperature"),
+                 Humidity= request.form.get("Humidity"),
+                 Wind_Speed =request.form.get("Wind_Speed"),
+                 Precipitation =request.form.get("Precipitation"),
+                 Cloud_Cover =request.form.get("Cloud_Cover"),
+                 Atmospheric_Pressure =request.form.get("Atmospheric_Pressure"),
+                 UV_Index =request.form.get("UV_Index"),
+                 Season =request.form.get("Season"),
+                 Visibility_km =request.form.get("Visibility_km"),
+                 Location =request.form.get("Location")
         )
         #now convert into the DataFrame by the function 
         df=DATA.get_data_DataFrame()
@@ -34,3 +34,7 @@ def predict_datapoints():
 
         result=pred
         return render_template("result.html",final_result=result)
+    
+
+if __name__=="__main__":
+    app.run(debug=True,host='0.0.0.0', port=8080)
